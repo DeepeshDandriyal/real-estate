99a75a5f5a6e4e4f2592e9dff85237a5506217b9
Deepesh Dandriyal <deepeshdandriyal1@gmail.com>
2025-06-12 10:46:50 +0530

    Header created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import Header from "./components/Header";
 
 const App = () => {
-  return <div className="text-blue-500">App</div>;
+  return (
+    <div className="w-full overflow-hidden">
+      <Header />
+    </div>
+  );
 };
 
 export default App;
